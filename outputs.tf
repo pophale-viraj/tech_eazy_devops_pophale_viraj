2035c364c6c7fc01e288f5ce1dbb22e2a38cb8a6
pophale-viraj <viraj.pophale.0412@gmail.com>
2025-09-26 17:52:54 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
 output "ip_address_1" {
   value = aws_instance.up-server-0412[*].private_ip
 }
+
+output "s3_bucket_id" {
+  value = aws_s3_bucket.this.id
+}
+
+output "s3_bucket_arn" {
+  value = aws_s3_bucket.this.arn
+}
+
+output "s3_read_role_arn" {
+  value = aws_iam_role.s3_read_role.arn
+}
+
+output "s3_admin_role_arn" {
+  value = aws_iam_role.s3_admin_role.arn
+}
